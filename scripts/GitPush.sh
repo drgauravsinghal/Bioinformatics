ea51ad8ca7920e4a2bb15de80e6b00285fcedf2b
Ubuntu <ubuntu@ip-172-31-42-253.us-east-2.compute.internal>
2025-07-18 10:22:07 -0500

    Initial commit of Bioinformatics project

---
+++ scripts/GitPush.sh
@@ -0,0 +1,5 @@
+cd ~/Bioinformatics          ## Change path as needed
+git init
+git add .
+git commit -m "Initial commit of Bioinformatics project"       ## Change as needed
+git push -u origin main
